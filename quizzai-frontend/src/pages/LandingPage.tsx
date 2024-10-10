472d587bc45efcff1b65070b4dbb71c182a2cbc8
Guilherme Kuhn <gvvkuhn@gmail.com>
2024-10-10 17:48:24 -0300

    feat: Started landing page development, imported project logo and created custom input component

--- quizzai-frontend/src/pages/LandingPage.tsx
+++ quizzai-frontend/src/pages/LandingPage.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import LogoPrincipal from "../assets/logoprincipal.png";
 
 const LandingPage = () => {
-  return <h1>LandingPage</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        width: "100vw",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        background: "linear-gradient(to right, #A852C3, #56106D)",
+        gap: "3vh",
+      }}
+    >
+      <img src={LogoPrincipal} alt="Logo Principal" style={{ height: "20%" }} />
+      <h1 style={{ color: "white", fontWeight: "lighter" }}>
+        Otimize seus estudos usando Quizz.ai!
+      </h1>
+    </div>
+  );
 };
 
 export default LandingPage;

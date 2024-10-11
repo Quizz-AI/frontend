31193626fdcb7ac58a27614c66a4899218502911
Guilherme Kuhn <guilherme.kuhn002@edu.pucrs.br>
2024-10-11 19:13:28 -0300

    feat: Finished landing page

--- quizzai-frontend/src/pages/LandingPage.tsx
+++ quizzai-frontend/src/pages/LandingPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LogoPrincipal from "../assets/logoprincipal.png";
-import { CustomInput, DifficultyButton } from "../components";
+import { CustomInput, DifficultyButton, MainHeader } from "../components";
 
 const LandingPage = () => {
   const [difficulty, setDifficulty] = React.useState<string>("Easy");
@@ -11,41 +11,56 @@ const LandingPage = () => {
         display: "flex",
         height: "100vh",
         width: "100vw",
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: "flex-start",
         flexDirection: "column",
         background: "linear-gradient(to right, #A852C3, #56106D)",
-        gap: "3vh",
+        gap: "10vh",
       }}
     >
-      <img src={LogoPrincipal} alt="Logo Principal" style={{ height: "20%" }} />
-      <h1 style={{ color: "white", fontWeight: "lighter" }}>
-        Otimize seus estudos usando Quizz.ai!
-      </h1>
-      <CustomInput style={{ width: "50vw", height: "8vh" }} />
+      <MainHeader />
       <div
         style={{
           display: "flex",
+          justifyContent: "center",
+          justifySelf: "flex-end",
           alignItems: "center",
-          justifyContent: "space-evenly",
-          width: "30vw",
+          flexDirection: "column",
+          gap: "3vh",
         }}
       >
-        <DifficultyButton
-          difficulty="Easy"
-          isToggled={difficulty === "Easy"}
-          setDifficulty={() => setDifficulty("Easy")}
+        <img
+          src={LogoPrincipal}
+          alt="Logo Principal"
+          style={{ width: "30vw" }}
         />
-        <DifficultyButton
-          difficulty="Medium"
-          isToggled={difficulty === "Medium"}
-          setDifficulty={() => setDifficulty("Medium")}
-        />
-        <DifficultyButton
-          difficulty="Hard"
-          isToggled={difficulty === "Hard"}
-          setDifficulty={() => setDifficulty("Hard")}
-        />
+        <h1 style={{ color: "white", fontWeight: "lighter" }}>
+          Enhance your learning with Quizz.ai!
+        </h1>
+        <CustomInput style={{ width: "50vw", height: "8vh" }} />
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            width: "30vw",
+          }}
+        >
+          <DifficultyButton
+            difficulty="Easy"
+            isToggled={difficulty === "Easy"}
+            setDifficulty={() => setDifficulty("Easy")}
+          />
+          <DifficultyButton
+            difficulty="Medium"
+            isToggled={difficulty === "Medium"}
+            setDifficulty={() => setDifficulty("Medium")}
+          />
+          <DifficultyButton
+            difficulty="Hard"
+            isToggled={difficulty === "Hard"}
+            setDifficulty={() => setDifficulty("Hard")}
+          />
+        </div>
       </div>
     </div>
   );
